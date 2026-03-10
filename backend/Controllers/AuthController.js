d277224f22c8969fffe61f349153b6f9ee507384
Bigan Kumar <bigan6204@gmail.com>
2026-03-11 01:22:56 +0530

    Initial MERN Auth Project

---
+++ backend/Controllers/AuthController.js
@@ -0,0 +1,42 @@
+const UserModel = require("../Models/user");
+const bcrypt = require("bcryptjs");
+
+const signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const user = await UserModel.findOne({ email });
+
+        if (user) {
+            return res.status(409).json({
+                message: "User already exists, please login",
+                success: false
+            });
+        }
+
+        const userModel = new UserModel({
+            name,
+            email,
+            password
+        });
+
+        userModel.password = await bcrypt.hash(password, 10);
+
+        await userModel.save();
+
+        res.status(201).json({
+            message: "Signup successful",
+            success: true
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Internal server error",
+            success: false
+        });
+    }
+};
+
+module.exports = {
+    signup
+};
